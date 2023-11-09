ffb631a9b1b5f782eb546625557d4b915ccd5d05
dmarie777 <deimarie777@gmail.com>
2023-11-08 20:47:29 -0500

    Added tests for favoriteBlog and mostBlogs

--- BlogBack/utils/list_helper.js
+++ BlogBack/utils/list_helper.js
@@ -1,8 +1,32 @@
+var _ = require('lodash')
 
 const totalLikes = (objList) => {
     return objList.reduce((sum, b) => sum + b.likes, 0)
 }
 
+const favoriteBlog = (objList) => {
+    const mostLiked =objList.sort((a,b) => b.likes - a.likes)[0] 
+    return {
+        title: mostLiked.title, 
+        author: mostLiked.author, 
+        likes: mostLiked.likes
+    }
+}
+
+const mostBlogs = (objList) => {
+    const group = _.countBy(objList, 'author');
+    const mostBlogs = Object.entries(group).sort((a,b) => b[1] - a[1])[0];
+    const result = {
+        author: mostBlogs[0], 
+        blogs: mostBlogs[1]
+    };
+    return result;
+}
+
+
+
 module.exports = {
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
 }
