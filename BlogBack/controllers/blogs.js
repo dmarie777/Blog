e7f9450671b0e23d36e4822981686c56ac0e9e26
dmarie777 <deimarie777@gmail.com>
2023-11-22 22:20:20 -0500

    Adding async/await to controllers

--- BlogBack/controllers/blogs.js
+++ BlogBack/controllers/blogs.js
@@ -1,49 +1,51 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogRouter.get('/', (request, response) => {
-    Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
-      .catch(err => console.log(err))
-  })
+blogRouter.get('/', async (request, response) => {
+    try {
+      const blogs = await Blog.find({})
+      response.json(blogs)
+    } catch (err) {
+      console.log(err)
+    }
+})
 
-
-blogRouter.put('/:id', (request, response) => {
+blogRouter.put('/:id', async(request, response) => {
   const blog = {
     title: request.body.title,
     author: request.body.author,
     url: request.body.url,
     likes: request.body.likes
   }
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedBlog => {
-      response.json(updatedBlog)
-    })
-    .catch(err => console.log(err))
+  try {
+    const updatedBlog = Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(updatedBlog)
+  } catch (err) {
+    console.log(err)
+  }
 })
 
-blogRouter.post('/', (request, response) => {
+blogRouter.post('/', async (request, response) => {
   if (!request.body.likes) request.body.likes = 0; 
   if (!request.body.title || !request.body.url) {
     return response.status(400).end()
   } 
+  try {
     const blog = new Blog(request.body)
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+    const newblog = await blog.save()
+    response.status(201).json(blog)
+  } catch (err) {
+    console.log(err)
+  }
   })
 
-blogRouter.delete(`/:id`, (request, response) => {
-  Blog.findByIdAndDelete(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(err => next(err))
+blogRouter.delete(`/:id`, async (request, response) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } catch (err){
+    console.log(err)
+  }
 })
 
 module.exports = blogRouter
